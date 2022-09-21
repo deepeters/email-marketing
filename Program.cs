d60ed713ca5fcd1da9a3df18b25f5c547cedd571
deepeters <dennis95peters@gmail.com>
2022-09-21 06:32:58 +0300

    Log number of retries in trying to fetch data from an API

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace Viagogo
     //Create a model
     public class City
     {
-        public string CityName  { get; set; }
+        public string CityName { get; set; }
         public int Distance { get; set; }
         public string Event { get; set; }
     }
@@ -42,55 +42,52 @@ namespace Viagogo
                 };
             //1. find out all events that arein cities of customer
             // then add to email.
-            var customer = new Customer { 
-                Name = "Mr. Fake", 
-                City = "New York" 
+            var customer = new Customer
+            {
+                Name = "Mr. Fake",
+                City = "New York"
             };
 
-       
-            //var nearCities = GetDistance("New York", "Boston");
+            //If GetDistance() is expensive or could fai or time out, we provide a try catch to handle failure and a retry to fetch data again whenever there is failure. 
 
-            //    var cityEvents = events.Where(e => e.City == customer.City).ToList();
-
-            //    foreach (var item in cityEvents)
-            //    {
-            //        AddToEmail(customer, item);
-            //    }
-            //}
-
-            // 1. TASK
-
-
-            var cityNears = new List<City>();
-
-
-
-            var Cities = events.Select(x => new
-           City {
-                CityName=x.City,
-                Distance=0,
-                Event=x.Name
-
-            }).Distinct().ToList();
-
-            foreach (var city in Cities)
+            try
             {
-                cityNears.Add(new City {CityName= city.CityName,Event=city.Event,Distance= GetDistance("New York", city.CityName)});
+                var cityNears = new List<City>();
+
+                var Cities = events.Select(x => new
+                City
+                {
+                    CityName = x.City,
+                    Distance = 0,
+                    Event = x.Name
+
+                }).Distinct().ToList();
+
+                if (ShouldRetryHandler.shouldRetry())
+                {
+                    foreach (var city in Cities)
+                    {
+                        cityNears.Add(new City
+                        {
+                            CityName = city.CityName,
+                            Event = city.Event,
+                            Distance = GetDistance("New York", city.CityName)
+                        });
+                    }
+                }
+
+                var nearest = cityNears.OrderBy(x => x.Distance).Take(5);
+
+                foreach (var item in nearest)
+                {
+                    AddToEmail(customer, new Event { Name = item.Event, City = item.CityName });
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
             }
 
-            var nearest = cityNears.OrderBy(x => x.Distance).Take(5);
-
-            foreach (var item in nearest)
-            {
-                AddToEmail(customer, new Event {Name= item.Event,City=item.CityName });
-            }
-
-            //Improvement: Send a single email with all events rather than multiple emails for each event.
-
-            /*
-            * We want you to send an email to this customer with all events in their city
-            * Just call AddToEmail(customer, event) for each event you think they should get
-            */
 
 
         }
@@ -116,16 +113,43 @@ namespace Viagogo
             var i = 0;
             for (i = 0; i < Math.Min(s.Length, t.Length); i++)
             {
-                // Console.Out.WriteLine($"loop 1 i={i} {s.Length} {t.Length}");
+                //Console.Out.WriteLine($"loop 1 i={i} {s.Length} {t.Length}");
                 result += Math.Abs(s[i] - t[i]);
             }
             for (; i < Math.Max(s.Length, t.Length); i++)
             {
-                // Console.Out.WriteLine($"loop 2 i={i} {s.Length} {t.Length}");
+                //Console.Out.WriteLine($"loop 2 i={i} {s.Length} {t.Length}");
                 result += s.Length > t.Length ? s[i] : t[i];
             }
             return result;
         }
+
+        internal class ShouldRetryHandler
+        {
+            private static int RETRIES_MAX_NUMBER = 3;
+            private static int numberTrys;
+
+            public static bool shouldRetry()
+            {
+                var statusRetry = false;
+
+                if (numberTrys < RETRIES_MAX_NUMBER)
+                {
+                    numberTrys++;
+                    statusRetry = true;
+                    //Increment the number of trials to fetch the data from the GetDistance() API
+                }
+
+                else
+                {
+                    statusRetry = false;
+                    //Reached retry number limit
+                }
+
+                return statusRetry;
+            }
+        }
+
     }
 }
 /*
