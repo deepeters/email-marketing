23f65afd8534e2965fb3fd2fc69c2a42e3321916
deepeters <dennis95peters@gmail.com>
2022-09-21 05:29:34 +0300

    Implement try catch to handle API calls that may fail

--- Program.cs
+++ Program.cs
@@ -47,50 +47,38 @@ namespace Viagogo
                 City = "New York" 
             };
 
-       
-            //var nearCities = GetDistance("New York", "Boston");
+            //If GetDistance() is expensive or could fai or time out, we provide a try catch to handle failure and a retry to fetch data again whenever there is failure. 
 
-            //    var cityEvents = events.Where(e => e.City == customer.City).ToList();
-
-            //    foreach (var item in cityEvents)
-            //    {
-            //        AddToEmail(customer, item);
-            //    }
-            //}
-
-            // 1. TASK
-
-
-            var cityNears = new List<City>();
-
-
-
-            var Cities = events.Select(x => new
-           City {
-                CityName=x.City,
-                Distance=0,
-                Event=x.Name
-
-            }).Distinct().ToList();
-
-            foreach (var city in Cities)
+            try
             {
-                cityNears.Add(new City {CityName= city.CityName,Event=city.Event,Distance= GetDistance("New York", city.CityName)});
+                var cityNears = new List<City>();
+
+                var Cities = events.Select(x => new
+                City
+                {
+                    CityName = x.City,
+                    Distance = 0,
+                    Event = x.Name
+
+                }).Distinct().ToList();
+
+                foreach (var city in Cities)
+                {
+                    cityNears.Add(new City { CityName = city.CityName, Event = city.Event, Distance = GetDistance("New York", city.CityName) });
+                }
+
+                var nearest = cityNears.OrderBy(x => x.Distance).Take(5);
+
+                foreach (var item in nearest)
+                {
+                    AddToEmail(customer, new Event { Name = item.Event, City = item.CityName });
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.ToString(), "Get Distance fetch request Timed Out");
             }
 
-            var nearest = cityNears.OrderBy(x => x.Distance).Take(5);
-
-            foreach (var item in nearest)
-            {
-                AddToEmail(customer, new Event {Name= item.Event,City=item.CityName });
-            }
-
-            //Improvement: Send a single email with all events rather than multiple emails for each event.
-
-            /*
-            * We want you to send an email to this customer with all events in their city
-            * Just call AddToEmail(customer, event) for each event you think they should get
-            */
 
 
         }
@@ -116,12 +104,12 @@ namespace Viagogo
             var i = 0;
             for (i = 0; i < Math.Min(s.Length, t.Length); i++)
             {
-                // Console.Out.WriteLine($"loop 1 i={i} {s.Length} {t.Length}");
+                //Console.Out.WriteLine($"loop 1 i={i} {s.Length} {t.Length}");
                 result += Math.Abs(s[i] - t[i]);
             }
             for (; i < Math.Max(s.Length, t.Length); i++)
             {
-                // Console.Out.WriteLine($"loop 2 i={i} {s.Length} {t.Length}");
+                //Console.Out.WriteLine($"loop 2 i={i} {s.Length} {t.Length}");
                 result += s.Length > t.Length ? s[i] : t[i];
             }
             return result;
